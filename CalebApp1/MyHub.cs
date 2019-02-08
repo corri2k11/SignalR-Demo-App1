4b787ce63bb133f08030c545fe5b67ddb8eafecd
CARLOS ALCANTARA CORRIPIO <carlos.corripio@ad.infosys.com>
2019-02-08 00:27:31 -0600

    Refactored client and server side code

--- CalebApp1/MyHub.cs
+++ CalebApp1/MyHub.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace CalebApp1
 {
+    [HubName("theHub")]
     public class MyHub : Hub
     {
+        [HubMethodName("announceToEverybody")]
         public void Announce(string message)
         {
+            //Broadcast to all connected clients, everybody
             Clients.All.Announce(message);
+        }
+
+        //One way of doing this method...
+        //public void GetServerDateTime()
+        //{
+        //    //Broadcast to only the client who sent the request
+        //    Clients.Caller.DisplayDateTime();
+        //}
+
+        //Another way of doing the same, using a return type...
+
+        public DateTime GetServerDateTime()  //Pascal casing
+        {
+            return DateTime.Now;
         }
     }
 }
--- CalebApp1/Scripts/mySignalrClient.js
+++ CalebApp1/Scripts/mySignalrClient.js
@@ -1,19 +1,38 @@
 ﻿//Creating an Inmediately Invoked Function Expression
 //Basically creatinga  function in 1st segment, and invoking it in the second ()()
-(function () {
-    $.connection.hub
-        .start()                    //opens a connection to signalr hub
-        .done(function () {         //successful connection, works do something...
-            console.log("Connected to SignalR Hub");
-            //invoke server side (Hub) announce method, pass in some message/data
-            $.connection.myHub.server.announce("Connected!");
-        })
-        .fail(function () {         //failed connection
-            alert("Failed connecting to SignalR Hub");
-        })
 
-    $.connection.myHub.client.announce = function (msg) {
-        //alert(msg);
+(function () {
+    var theHub = $.connection.theHub;
+
+    $("#btnClickMe").on("click", function () {
+        //invokes server side (Hub) announce method, pass in some message/data
+        theHub.server.announceToEverybody("Test message");
+    });
+    $("#btnGetServerDate").on("click", function () {
+        theHub.server.getServerDateTime()   //returns a Promise object from the call to the server/hub method. Data from the Promise can be retrievied in the done & fail callback methdos
+                     .done(function (data) {                     //callback function, function that's run after we get the data
+                        writeToPage(data);
+                     })
+                     .fail(function (err) {
+                        writeToPage(err);
+                     })
+    })
+    var writeToPage = function (msg) {
         $("#divWelcomeMsg").append(msg + "<br>");
     }
+    theHub.client.announce = function (msg) {
+        writeToPage(msg);
+    }
+
+    //opens a connection to signalr hub. Does something on successfull or failure callback methods (done & fail respectively)
+    $.connection.hub
+                .start()                                         
+                .done(function () { 
+                    $.connection.hub.logging = true; //enable the built in logging. starts a console log for signalr         
+                    $.connection.hub.log("Connected to Hub...");
+                    writeToPage("Connected to Hub...")                
+                })
+                .fail(function () {         
+                    writeToPage("Failed connecting to SignalR Hub");
+                })
 })()
